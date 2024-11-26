2cef8d98c00b2800c4b1df686d21ab4e8e413576
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-26 18:05:40 -0300

    lógica do jogo #01

---
+++ src/functions.tsx
@@ -0,0 +1,36 @@
+const createBoard = (rows, columns) => {
+    return Array(rows).fill(0).map((_, row) => {
+        return Array(columns).fill(0).map((_, column) => {
+            return {
+                row, 
+                column,
+                opened: false,
+                flagged: false,
+                mined: false,
+                exploded: false,
+                nearMines: 0,
+            }
+        })
+    })
+}
+
+const spreadMines = (board, minesAmount) => {
+    const rows = board.lengh
+    const colums = board[0].lengh
+    let minesPlanted = 0
+    while (minesPlanted < minesAmount) {
+        const rowSel = parseInt(Math.random() * rows, 10)
+        const columnSel = parseInt(Math.random() * colums, 10)
+
+        if(!board[rowSel][columnSel].mined){
+            board[rowSel][columnSel].mined = true
+            minesPlanted++
+        }
+    }
+}
+
+const createMinedBoard = (rows, columns, minesAmount) => {
+    const board = createBoard(rows, columns)
+    spreadMines(board, minesAmount)
+    return board
+}
